dd1820e9c17ab7a6b3fa967ca60fb0239b1c9450
JonathanSpalding <Jon.Spalding@hotmail.com>
2016-04-20 11:15:48 -0600

    CS 1030 C#

---
+++ SoloPong/frmMain.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace SoloPong
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblPoints = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.pnlPaddle = new System.Windows.Forms.Panel();
+            this.picBall = new System.Windows.Forms.PictureBox();
+            this.Timer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBall)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 239);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Points: ";
+            // 
+            // lblPoints
+            // 
+            this.lblPoints.AutoSize = true;
+            this.lblPoints.Location = new System.Drawing.Point(60, 239);
+            this.lblPoints.Name = "lblPoints";
+            this.lblPoints.Size = new System.Drawing.Size(13, 13);
+            this.lblPoints.TabIndex = 1;
+            this.lblPoints.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(95, 239);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Time: ";
+            // 
+            // lblTime
+            // 
+            this.lblTime.AutoSize = true;
+            this.lblTime.Location = new System.Drawing.Point(137, 239);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(13, 13);
+            this.lblTime.TabIndex = 3;
+            this.lblTime.Text = "0";
+            // 
+            // pnlPaddle
+            // 
+            this.pnlPaddle.BackColor = System.Drawing.Color.Blue;
+            this.pnlPaddle.Location = new System.Drawing.Point(25, 93);
+            this.pnlPaddle.Name = "pnlPaddle";
+            this.pnlPaddle.Size = new System.Drawing.Size(10, 40);
+            this.pnlPaddle.TabIndex = 4;
+            // 
+            // picBall
+            // 
+            this.picBall.Location = new System.Drawing.Point(2, 3);
+            this.picBall.Name = "picBall";
+            this.picBall.Size = new System.Drawing.Size(20, 20);
+            this.picBall.TabIndex = 5;
+            this.picBall.TabStop = false;
+            // 
+            // Timer
+            // 
+            this.Timer.Interval = 10;
+            this.Timer.Tick += new System.EventHandler(this.Timer_Tick);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.picBall);
+            this.Controls.Add(this.pnlPaddle);
+            this.Controls.Add(this.lblTime);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblPoints);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmMain";
+            this.Text = "Solo Pong";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.frmMain_MouseMove);
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBall)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.BindingSource bindingSource1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblPoints;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.Panel pnlPaddle;
+        private System.Windows.Forms.PictureBox picBall;
+        private System.Windows.Forms.Timer Timer;
+    }
+}
+
---
+++ SoloPong/frmMain.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SoloPong
+{
+    public partial class frmMain : Form
+    {
+        int x = 1, y = 1, deltaX = 3, deltaY = 3;
+        Bitmap ball;
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            // Crate ball bitmap image
+            ball = new Bitmap(picBall.Width, picBall.Height); // create blank bitmap
+            Graphics gr = Graphics.FromImage(ball); // Drawing tool for ball
+            SolidBrush br = new SolidBrush(Color.Red); // create new Red brush
+            gr.FillEllipse(br, 1, 1, ball.Width - 1, ball.Height - 1); // Draw Ball
+            
+            picBall.Image = ball; // Assign ball to picBall.Image
+            Timer.Enabled = true; //enable timer
+        }
+
+        private void frmMain_MouseMove(object sender, MouseEventArgs e)
+        {
+            this.pnlPaddle.Location = new System.Drawing.Point
+                (pnlPaddle.Location.X, e.Y - pnlPaddle.Height / 2);
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            // compares below will be made against the middle of the ball
+            int middle = picBall.Location.Y + picBall.Height / 2;
+            //Check to see if picBall is moving left and hit paddle
+            if (deltaX < 0 &&
+            middle > pnlPaddle.Location.Y
+            && middle < pnlPaddle.Location.Y + pnlPaddle.Height
+            && picBall.Location.X <= pnlPaddle.Location.X)
+            {
+                // Place code here to process paddle hits
+                // paddle was hit
+                deltaX = -1 * deltaX; // change X direction to moving right
+                int score = int.Parse(lblPoints.Text) + 1;
+                lblPoints.Text = score.ToString(); // increment score
+            }
+            else
+            {
+                // Place code here to deal with
+                // hitting game area sides
+                // Check to see if ball missed paddle and hit left side
+                if (picBall.Location.X <= 0)
+                {
+                    deltaX = -1 * deltaX; // Change X direction and
+                    int score = int.Parse(lblPoints.Text) - 1; // decrement score
+                    lblPoints.Text = score.ToString();
+                }
+                // Check to see if ball hit right side
+                if (picBall.Location.X >= (this.ClientSize.Width - picBall.Width))
+                {
+                    deltaX = -1 * deltaX; // change X direction
+                }
+                // Check to see if picBall hit the top or bottom
+                if (picBall.Location.Y <= 0 ||
+                picBall.Location.Y >= (this.ClientSize.Height - picBall.Height))
+                {
+                    deltaY = -1 * deltaY; // Change Y direction
+                }
+            }
+            // Place code here to actually move the ball.
+            // Update picBall location
+            x = picBall.Location.X + deltaX;
+            y = picBall.Location.Y + deltaY;
+            this.picBall.Location = new System.Drawing.Point(x, y);
+            lblTime.Text = (float.Parse(lblTime.Text) + 0.01f).ToString("#0.00");
+
+        }
+
+
+
+    }
+}
